66854ce546aed93f27417a6f87609e9d972b98ba
Thulasi <atfornet@gmail.com>
2026-06-08 13:38:29 -0700

    Max Solitaitre Moves - Stack Pattern

---
+++ stack/max-solitaire-moves.js
@@ -0,0 +1,43 @@
+function maxSolitaireMoves(cards) {
+    if (!cards || cards.length < 2) {
+        return 0;
+    }
+
+    let moves = 0;
+    let stackCards = [cards[0]];
+
+    for (let i = 1; i < cards.length; i++) {
+        let nextCard = cards[i];
+        let topCard = stackCards[stackCards.length - 1];
+
+        if (nextCard.rank === topCard.rank - 1 && nextCard.color !== topCard.color) {
+            moves++;
+            stackCards[stackCards.length - 1] = nextCard;
+        } else {
+            stackCards.push(nextCard);
+        }
+    }
+
+    return moves;
+}
+
+const cards = [
+    { rank: 7, color: "black" },
+    { rank: 6, color: "red" },
+    { rank: 5, color: "black" },
+    { rank: 9, color: "red" }
+];
+
+const cards2 = [
+    { rank: 8, color: "black" },
+    { rank: 7, color: "red" },
+    { rank: 6, color: "red" },
+    { rank: 5, color: "black" }
+];
+console.log(maxSolitaireMoves(cards));
+
+// > maxSolitaireMoves(cards)
+// > 2 // 6 onto 7, 5 onto 6
+
+// > maxSolitaireMoves(cards2)
+// > 2 // 7 onto 8, 5 onto 6
