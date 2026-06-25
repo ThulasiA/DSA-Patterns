7fdec33fe12475583ad789b183cb6dad5ae87ceb
Thulasi <atfornet@gmail.com>
2026-06-24 22:40:36 -0700

    Add Jump game two

--- greedy/jump-game.js
+++ greedy/jump-game.js
@@ -25,3 +25,26 @@ function jumpGame(nums) {
 // const nums = [1,2,0,1,0]
 // const nums2 = [3,2,1,0,4]
 // console.log(jumpGame(nums))
+
+// https://leetcode.com/problems/jump-game-ii/description/
+
+function jumpGameTwo(nums) {
+    let res = 0;
+    let l = 0;
+    let r = 0;
+
+    while (r < nums.length - 1) {
+        let farthest = 0;
+        for (let i = 0; i <= r; i++) {
+            farthest = Math.max(farthest, i + nums[i]);
+        }
+        l = r + 1;
+        r = farthest;
+        res++;
+    }
+
+    return res;
+}
+
+//const nums = [2,4,1,1,1,1]
+//console.log(jumpGameTwo(nums))
